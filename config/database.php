bc3aed6da94b15a57edfc7a58a0c68e112f4d788
Mohammad Mehrabani <mehrabani.developer@gmail.com>
2023-08-17 00:41:50 +0330

    enabled mongodb replicate to use transaction functionality

--- config/database.php
+++ config/database.php
@@ -95,13 +95,14 @@ return [
 
         'mongodb' => [
             'driver' => 'mongodb',
-            'host' => env('DB_HOST'),
+            'host' => env('DB_HOST'), /*['mongo1', 'mongo2']*/
             'port' => env('DB_PORT'),
             'database' => env('DB_DATABASE'),
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
             'options' => [
                 'database' => env('DB_AUTHENTICATION_DATABASE', 'admin'),
+                'replicaSet' => env('MONGODB_REPLICA_SET', 'rs')
             ],
         ],
 
